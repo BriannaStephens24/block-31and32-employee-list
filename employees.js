2cc744ce1a10470564b7d8a6034f0ddfd718c45e
Brianna Stephens <briannaastephens24@gmail.com>
2025-02-18 07:21:31 -0700

    initial commit

---
+++ employees.js
@@ -0,0 +1,24 @@
+
+const employees = [
+  { id: 1, 
+    name: "Meredith Grey", 
+   },
+
+  { id: 2, 
+    name: "Miranda Bailey", 
+   },
+
+  { id: 3, 
+    name: "Derek Shepherd", 
+   },
+
+  { id: 4, 
+    name: "Alex Karev", 
+   },
+
+  { id: 5, 
+    name: "Christina Yang", 
+   },
+];
+
+module.exports = employees;
