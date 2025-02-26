b6581f4cd669fec1bc0e9e85318cf748484064cd
Brianna Stephens <briannaastephens24@gmail.com>
2025-02-26 11:44:41 -0700

    added render link

--- employee-directory/src/EmployeeList.jsx
+++ employee-directory/src/EmployeeList.jsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 const EmployeeList = () => {
   useEffect(() => {
     const getEmployees = async() => {
-      const response = await fetch(' --- link from render');
+      const response = await fetch('https://block-31and32-employees.onrender.com/');
       console.log(response);
     }
     getEmployees();
